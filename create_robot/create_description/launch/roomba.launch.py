dd95ff7942c6c6fd6abe5303d8640d1fc0cf14ee
Kousuke Takeuchi <u651601f@gmail.com>
2021-02-08 14:17:36 +0900

    update launch to show rviz

--- create_robot/create_description/launch/roomba.launch.py
+++ create_robot/create_description/launch/roomba.launch.py
@@ -1,27 +1,35 @@
 import os
-from glob import glob
+
+from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
 from launch_ros.actions import Node
-from launch.substitutions import LaunchConfiguration, Command
-from launch.actions import ExecuteProcess
+from launch_ros.substitutions import FindPackageShare
+
+import xacro
 
 
-# Launch an emulator for testing
+# https://qiita.com/hakuturu583/items/7e3a278630422e17f0ba
+share_dir_path = os.path.join(get_package_share_directory('create_description'))
+xacro_path = os.path.join(share_dir_path, 'urdf', 'create_2.urdf.xacro')
+urdf_path = os.path.join(share_dir_path, 'urdf', 'create_2.urdf')
 
 
 def generate_launch_description():
-    xacro_path = os.path.join('share', 'create_description', 'urdf/create_2.urdf.xacro')
-    print(xacro_path)
-    roomba_description_params = [
-        {'robot_description': Command(['xacro',' ', xacro_path])}
-    ]
+    # xacroをロード
+    doc = xacro.process_file(xacro_path)
+    # xacroを展開してURDFを生成
+    robot_desc = doc.toprettyxml(indent='  ')
+    # urdf_pathに対してurdfを書き出し
+    with open(urdf_path, 'w+') as f:
+      f.write(robot_desc)
+      f.close()
 
     return LaunchDescription([
         Node(package='robot_state_publisher',
              executable='robot_state_publisher',
-             node_name='robot_state_publisher',
-             parameters=roomba_description_params,
+             name='robot_state_publisher',
+             arguments=[urdf_path],
              output='screen'
         ),
     ])
